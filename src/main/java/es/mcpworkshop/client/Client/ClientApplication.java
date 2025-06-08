b888861881f6674c02a5771454a3daa8a2ea9f10
jose luis sanchez romero <muallin@gmail.com>
2025-06-09 00:26:08 +0200

    Use openweather

--- src/main/java/es/mcpworkshop/client/Client/ClientApplication.java
+++ src/main/java/es/mcpworkshop/client/Client/ClientApplication.java
@@ -1,36 +1,74 @@
 package es.mcpworkshop.client.Client;
 
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
-public class ClientApplication {
+public class ClientApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(ClientApplication.class, args);
   }
 
-  @Bean
-  public CommandLineRunner predefinedQuestions(
-      ChatClient.Builder chatClientBuilder,
-      ToolCallbackProvider tools,
-      ConfigurableApplicationContext context) {
+  @Autowired
+  private ChatClient chatClient;
 
-    return args -> {
-      if (args.length > 0) {
-        var chatClient = chatClientBuilder.defaultToolCallbacks(tools).build();
-
-        System.out.printf("%n>>> QUESTION: %s", Arrays.toString(args));
-        System.out.printf(
-            "%n>>> ASSISTANT: %s", chatClient.prompt(Arrays.toString(args)).call().content());
-        context.close();
+  @Override
+  public void run(String... args) {
+    String comando;
+    Scanner scanner = new Scanner(System.in);
+    do {
+      System.out.println("Ask a question: ( quit to terminate )");
+      comando = scanner.nextLine().trim();
+      if (!comando.equalsIgnoreCase("quit")) {
+        System.out.printf("%n>>> ASSISTANT: %s%n", chatClient.prompt(comando).call().content());
       }
-    };
+    } while (!comando.equalsIgnoreCase("quit"));
+    System.out.println("Bye bye !");
+    System.exit(0);
+  }
+
+  @Configuration
+  static class AIConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AIConfig.class);
+
+    @Value("classpath:enhanced-system-prompt.txt")
+    private Resource systemPrompt;
+
+    @Bean
+    public ChatClient chatClient(
+            ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, ToolCallbackProvider tools)
+            throws Exception {
+      LOG.info("Initializing ChatClient");
+      String systemText =
+              systemPrompt.getContentAsString(StandardCharsets.UTF_8).formatted(LocalDateTime.now());
+
+      return chatClientBuilder
+              .defaultAdvisors(
+                      List.of(
+                              new SimpleLoggerAdvisor(), MessageChatMemoryAdvisor.builder(chatMemory).build()))
+              .defaultSystem(systemText)
+              .defaultToolCallbacks(tools)
+              .build();
+    }
   }
 }
