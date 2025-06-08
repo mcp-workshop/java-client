5216d79dc4b8af7d3e0899dd773ca029880da355
jose luis sanchez romero <muallin@gmail.com>
2025-06-09 00:26:08 +0200

    Use openweather

--- src/main/java/es/mcpworkshop/client/Client/ClientApplication.java
+++ src/main/java/es/mcpworkshop/client/Client/ClientApplication.java
@@ -1,36 +1,33 @@
 package es.mcpworkshop.client.Client;
 
-import java.util.Arrays;
+import java.util.Scanner;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ClientApplication {
+public class ClientApplication implements CommandLineRunner {
+
+  @Autowired private ChatClient chatClient;
 
   public static void main(String[] args) {
     SpringApplication.run(ClientApplication.class, args);
   }
 
-  @Bean
-  public CommandLineRunner predefinedQuestions(
-      ChatClient.Builder chatClientBuilder,
-      ToolCallbackProvider tools,
-      ConfigurableApplicationContext context) {
-
-    return args -> {
-      if (args.length > 0) {
-        var chatClient = chatClientBuilder.defaultToolCallbacks(tools).build();
-
-        System.out.printf("%n>>> QUESTION: %s", Arrays.toString(args));
-        System.out.printf(
-            "%n>>> ASSISTANT: %s", chatClient.prompt(Arrays.toString(args)).call().content());
-        context.close();
+  @Override
+  public void run(String... args) {
+    String comando;
+    Scanner scanner = new Scanner(System.in);
+    do {
+      System.out.println("Ask a question: ( quit to terminate )");
+      comando = scanner.nextLine().trim();
+      if (!comando.equalsIgnoreCase("quit")) {
+        System.out.printf("%n>>> ASSISTANT: %s%n", chatClient.prompt(comando).call().content());
       }
-    };
+    } while (!comando.equalsIgnoreCase("quit"));
+    System.out.println("Bye bye !");
+    System.exit(0);
   }
 }
